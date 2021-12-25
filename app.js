af6595912ca178caa83464d822664e883b2ebe09
Alina <malinka2002@mail.ru>
2021-12-25 23:30:55 +0300

    layout

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = require("./src/routes.js");
 const app = express();
 
-app.use(express.static("./static"));
-
 app.set("views", "./templates");
 app.set("view engine", "hbs");
+
+app.use(express.static("./static"));
 
 app.use("/", router);
 
--- src/routes.js
+++ src/routes.js
@@ -7,4 +7,6 @@ router.get("/", (req,res) => {
     });
 })
 
+
+
 module.exports = router;
