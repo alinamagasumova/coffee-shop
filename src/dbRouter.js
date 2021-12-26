6314ed197de833bfb361eff38b98d3aca5e54b82
maripetrova <74290687+maripetrova@users.noreply.github.com>
2021-12-26 16:43:26 +0300

    Обновление

--- src/dbRouter.js
+++ src/dbRouter.js
@@ -2,5 +2,25 @@ const router = require("express").Router();
 const db = require("./db.js");
 const mongodb = require("mongodb");
 
+router.post("/admin", (req, res) => {
+    console.log(req.body)
+    let client = db();
+    client.connect(err => {
+        if (err) {
+        } else {
+            const table = client.db("CoffeeShop");
+            const col = table.collection("products");
+            col.insertOne(req.body, err => {
+                if (err) {
+                    console.log(err);
+                    client.close();
+                } else {
+                    client.close();
+                }
+            });
+        }
+    })
+    res.send({msg: "done"})
+})
 
 module.exports = router;
