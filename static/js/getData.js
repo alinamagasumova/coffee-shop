75021c6c8e87d583fae38ca50a0c352f6f736d98
Alina <malinka2002@mail.ru>
2021-12-29 13:36:51 +0300

    getData

--- static/js/getData.js
+++ static/js/getData.js
@@ -2,8 +2,7 @@ let container = document.querySelector('.products');
 
 const getData = async (param) => {
     const res = await fetch('/api/category/' + param);
-    const data = await res.text();
-    console.log(data);
+    const data = await res.json();
     data.data.forEach((obj) => {
         container.innerHTML += `<div class="products__item" data-key="${obj._id}">${obj.name}</div><div class='hide'></div>`;
     });
