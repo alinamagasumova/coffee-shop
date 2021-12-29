71b84fc9924f439e417c04d23d348273dc0e5513
Alina <malinka2002@mail.ru>
2021-12-29 15:40:16 +0300

    fast

--- static/js/getData.js
+++ static/js/getData.js
@@ -39,7 +39,11 @@ const getData = async (param) => {
         } else {
             container.innerHTML += `
             <div class="products__item" data-key="${obj._id}">
+                <img src="/images/main/${obj.photo}" alt="" class="img_coffee">
                 <h3 class="fontB data_header pa">${obj.name}</h3>
+                <p class="data_p pa">Степень обжарки: ${obj.step}</p>
+                <p class="data_p2 pa">Страна: ${obj.country}</p>
+                <p class="data_p3 pa">Вид кофе: ${obj.vid}</p>
                 <p class="data_p4 pa">${obj.description}</p>
                 <p class="data_p2 pa">${obj.typeTea}</p>
                 <p class="data_p2 pa">${obj.typeFood}</p>
